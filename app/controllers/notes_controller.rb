f9639314bdafda5dbc3888ba865eba169086bc03
Nathan McCallum <nathan@1vas.me>
2020-05-27 15:55:37 +1000

    UI for linking contacts/companies/projects to notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -43,6 +43,6 @@ class NotesController < ApplicationController
   private
 
   def note_params
-    params.require(:note).permit(:title, :content)
+    params.require(:note).permit(:title, :content, :contact_id, :company_id, :project_id)
   end
 end
