1423d7477004d8abb7ad81e910be2b2eb470a445
Nathan McCallum <nathan@1vas.me>
2020-06-03 20:53:49 +1000

    Implement profile updating

--- app/controllers/settings/profile_controller.rb
+++ app/controllers/settings/profile_controller.rb
@@ -2,4 +2,17 @@ class Settings::ProfileController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def update
+    @user = current_user
+    @user.update user_params
+    flash[:notice] = 'Profile successfully updated.'
+    render 'edit'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
 end
