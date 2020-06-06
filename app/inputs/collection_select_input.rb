081550842f04d5b9a7bc288bc3e99ccee00f6d4e
Nathan McCallum <nathan@1vas.me>
2020-06-07 00:55:12 +1000

    FIx multi select fields

--- app/inputs/collection_select_input.rb
+++ app/inputs/collection_select_input.rb
@@ -1,5 +1,13 @@
 class CollectionSelectInput < SimpleForm::Inputs::CollectionSelectInput
   def input(wrapper_options)
-    template.content_tag(:div, super, class: 'select is-fullwidth')
+    template.content_tag(
+      :div,
+      super,
+      class: "select is-fullwidth #{'is-multiple' if is_multiple?}"
+    )
+  end
+
+  def is_multiple?
+    input_options[:input_html][:multiple] rescue false
   end
 end
