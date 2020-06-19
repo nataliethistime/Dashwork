694be40b9042a19cca4f4cca9abb3c1d07dc3043
Nathan McCallum <nathan@1vas.me>
2020-06-20 00:30:20 +1000

    Autosize text areas

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+const autosize = require('autosize');
+
 require("@rails/ujs").start();
 require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
 require('local-time').start();
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -40,4 +39,9 @@ document.addEventListener('turbolinks:load', () => {
       });
     });
   }
+
+  //
+  // Autosize all <textarea> elements
+  //
+  autosize(document.querySelectorAll('textarea'));
 });
