8f671c7d33eaaf255fe5565c3768b6a04b1037a4
Nathan McCallum <nathan@1vas.me>
2020-06-26 23:12:38 +1000

    Extract custom field database logic to a concern
    First time trying this - it seems to work nicely

--- app/models/company.rb
+++ app/models/company.rb
@@ -29,36 +29,5 @@ class Company < ApplicationRecord
   default_scope -> { order(:name) }
   decorate_with CompanyDecorator
 
-  has_many :custom_values, class_name: 'Value', foreign_key: :linked_record_id, dependent: :destroy
-
-  accepts_nested_attributes_for :custom_values
-
-  def custom_fields
-    tenant.company_fields
-  end
-
-  def build_custom_values
-    existing_fields = custom_values.map(&:field_id)
-
-    custom_fields.each do |field|
-      unless existing_fields.include? field.id
-        custom_values.build field_id: field.id, type: field.type
-      end
-    end
-
-    custom_values
-  end
-
-  def custom_values_attributes=(attributes)
-    list = Hash[attributes.values.map { |attr| [attr['field_id'].to_s, attr['value']] }]
-
-    build_custom_values.each do |cv|
-      new_value = list[cv.field_id.to_s]
-      if new_value.present?
-        cv.value = new_value
-      else
-        cv.delete
-      end
-    end
-  end
+  include CustomFieldable
 end
---
+++ app/models/concerns/custom_fieldable.rb
@@ -0,0 +1,37 @@
+module CustomFieldable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :custom_values, class_name: 'Value', foreign_key: :linked_record_id, dependent: :destroy
+    accepts_nested_attributes_for :custom_values
+  end
+
+  def custom_fields
+    tenant.public_send "#{self.model_name.singular}_fields"
+  end
+
+  def build_custom_values
+    existing_fields = custom_values.map(&:field_id)
+
+    custom_fields.each do |field|
+      unless existing_fields.include? field.id
+        custom_values.build field_id: field.id, type: field.type
+      end
+    end
+
+    custom_values
+  end
+
+  def custom_values_attributes=(attributes)
+    list = Hash[attributes.values.map { |attr| [attr['field_id'].to_s, attr['value']] }]
+
+    build_custom_values.each do |cv|
+      new_value = list[cv.field_id.to_s]
+      if new_value.present?
+        cv.value = new_value
+      else
+        cv.delete
+      end
+    end
+  end
+end
