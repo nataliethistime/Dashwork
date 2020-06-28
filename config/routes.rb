3151ed6dde42db598c7b2470a87dc64a69f67934
Nathan McCallum <nathan@1vas.me>
2020-06-28 14:46:34 +1000

    Organise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,76 @@
 Rails.application.routes.draw do
-  resources :contacts
-  get '/contacts/:id/notes' => 'contacts#show_notes', as: :contact_notes
-  get '/contacts/:id/tasks' => 'contacts#show_tasks', as: :contact_tasks
-  get '/contacts/:id/projects' => 'contacts#show_projects', as: :contact_projects
-  resources :form_templates
-  resources :forms
+  root to: 'application#home'
+
+  #
+  # Administration
+  #
+  mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
+
+  #
+  # Authentication
+  #
+  devise_for :users
+
+  #
+  # Calendar
+  #
   resources :calendar_events
-  resources :equipment
+
+  #
+  # Companies
+  #
   resources :companies
   resources :company_fields, controller: 'fields', defaults: { domain: 'company' }
-  resources :contact_fields, controller: 'fields', defaults: { domain: 'contact' }
   resources :company_groups, controller: 'groups', defaults: { domain: 'company' }
-  resources :contact_groups, controller: 'groups', defaults: { domain: 'contact' }
   get '/companies/:id/notes' => 'companies#show_notes', as: :company_notes
   get '/companies/:id/contacts' => 'companies#show_contacts', as: :company_contacts
   get '/companies/:id/tasks' => 'companies#show_tasks', as: :company_tasks
   get '/companies/:id/projects' => 'companies#show_projects', as: :company_projects
-  resources :projects
-  devise_for :users
+
+  #
+  # Contacts
+  #
+  resources :contacts
+  resources :contact_fields, controller: 'fields', defaults: { domain: 'contact' }
+  resources :contact_groups, controller: 'groups', defaults: { domain: 'contact' }
+  get '/contacts/:id/notes' => 'contacts#show_notes', as: :contact_notes
+  get '/contacts/:id/tasks' => 'contacts#show_tasks', as: :contact_tasks
+  get '/contacts/:id/projects' => 'contacts#show_projects', as: :contact_projects
+
+  #
+  # Equipment
+  #
+  resources :equipment
+
+  #
+  # Forms
+  #
+  resources :form_templates
+  resources :forms
+
+  #
+  # Notes
+  #
   resources :notes
   resources :note_groups, controller: 'groups', defaults: { domain: 'note', display_as: 'tag' }
-  resources :tasks
+
+  #
+  # Projects
+  #
+  resources :projects
+
+  #
+  # Settings
+  #
   namespace :settings do
     resources :profile, only: [:edit, :update]
     resources :tenant, only: [:new, :create, :edit, :update] do
       get '/apps' => 'tenant#edit_apps'
     end
   end
-  mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
-  root to: 'application#home'
+
+  #
+  # Tasks
+  #
+  resources :tasks
 end
