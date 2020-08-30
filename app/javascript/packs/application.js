3d4f03faeb168d95c6f477d61406bfa02e895c2b
Nathan McCallum <nathan@1vas.me>
2020-08-30 14:52:15 +1000

    Tidy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,15 +6,8 @@
 const autosize = require('autosize');
 window.jQuery = window.$ = require('jquery');
 
-require("@rails/ujs").start();
-require("turbolinks").start();
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+require('@rails/ujs').start();
+require('turbolinks').start();
 
 document.addEventListener('turbolinks:load', () => {
   //
