cdc016779246d9bfc46183179af1f7dc2c8b1ab3
Nathan McCallum <nathan@1vas.me>
2020-09-22 19:52:09 +1000

    Decorate entry dates

--- app/decorators/personal_log_entry_decorator.rb
+++ app/decorators/personal_log_entry_decorator.rb
@@ -10,4 +10,8 @@ class PersonalLogEntryDecorator < DecoratorBase
   def day(personal_log_entry)
     localize personal_log_entry.date, format: '%A %B %e, %Y'
   end
+
+  def date(personal_log_entry)
+    format personal_log_entry.date, as: :date
+  end
 end
