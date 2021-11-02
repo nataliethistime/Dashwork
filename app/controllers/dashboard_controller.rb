e1f77265594e262891cb873dc113d2b554e313bd
Nathan McCallum <hey@nathan-mccallum.com>
2021-11-02 13:16:34 +1000

    Try showing tasks on the home screen (very basic)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
   def home
+    week_start = Time.zone.today.beginning_of_week(:sunday)
+    week_end = week_start + 7.days
     @entries = PersonalLog::Entry.recent
+    @tasks = Task.where(due_date: week_start..week_end)
   end
 
   private
