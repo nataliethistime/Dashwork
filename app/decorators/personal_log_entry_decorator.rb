beba0a637348279790464ed82ec54158fe6719f3
Nathan McCallum <hey@nathan-mccallum.com>
2021-11-04 00:45:04 +1000

    Change time format for log entries on home page

--- app/decorators/personal_log_entry_decorator.rb
+++ app/decorators/personal_log_entry_decorator.rb
@@ -7,6 +7,10 @@ class PersonalLogEntryDecorator < DecoratorBase
     localize personal_log_entry.entered_at, format: '%-l:%M %P'
   end
 
+  def entered_at_ago(personal_log_entry)
+    "#{helpers.time_ago_in_words(personal_log_entry.entered_at)} ago"
+  end
+
   def day(personal_log_entry)
     localize personal_log_entry.entered_at, format: '%A %B %e, %Y'
   end
