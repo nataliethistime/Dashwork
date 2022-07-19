2c5ec2efd10e3a00c9c560d402d32cc32c9b6c4c
Nathan McCallum <hey@nathan-mccallum.com>
2022-07-20 00:15:11 +1000

    Linking the whole db dir broke new migrations

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 lock "~> 3.16.0"
 
 append :linked_files, '.env'
+append :linked_files, 'db/production.sqlite3'
 append :linked_dirs, 'tmp'
 append :linked_dirs, 'log'
-append :linked_dirs, 'db'
 
 set :application, "dashwork"
 set :repo_url, "git@github.com:1vasari/Dashwork.git"
