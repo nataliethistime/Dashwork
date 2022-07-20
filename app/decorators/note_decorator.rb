03e3ecb6272557c94f9a5aea0d8357331ed91f0e
Nathan McCallum <hey@nathan-mccallum.com>
2022-07-20 17:33:28 +1000

    Fix notes preview

--- app/decorators/note_decorator.rb
+++ app/decorators/note_decorator.rb
@@ -1,10 +1,10 @@
 class NoteDecorator < DecoratorBase
-  def title(note)
-    helpers.truncate note.content, length: 60
-  end
-
   def content(note)
     format note.content, as: :markdown
+  end
+
+  def preview(note)
+    helpers.truncate note.content, length: 120
   end
 
   def author(note)
